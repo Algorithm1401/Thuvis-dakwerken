d6373682a12cc6d3af77237ef1eacccfe5b7323d
HungryDev <algorithm1401@gmail.com>
2021-04-05 21:57:23 +0200

    Aded pdf view & changed info

---
+++ src/app/vacature/vacature.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vacature',
+  templateUrl: './vacature.component.html',
+  styleUrls: ['./vacature.component.css']
+})
+export class VacatureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
